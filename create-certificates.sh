eccc79b0261f003c561689cc5f2a8a8c417cb32a
thedanmaor <133646682+thedanmaor@users.noreply.github.com>
2025-01-16 11:34:00 +0200

    Update create-certificates.sh

--- create-certificates.sh
+++ create-certificates.sh
@@ -42,4 +42,4 @@ kubectl create secret tls my-replica-sample-cert --cert=my-replica-sample.crt --
 kubectl create secret tls my-replica-sample-agent-certs --cert=my-replica-sample-agent.crt  --key=my-replica-sample-agent.key
 kubectl create configmap custom-ca --from-file=ca-pem
 
-echo "All Done"
+echo "Certificate Creation Completed Succesfully"
